905f62b22120fce61e4f5d46001d46cc5c935ff5
Likhita <leenupandagre24@gmail.com>
2022-07-27 09:51:50 +0530

    Generics comparing strings

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    public class Mainclass
+    {
+        private static void Main()
+        {
+            bool Equal = Calculator.AreEqual<string>("AB", "CD");
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+        }
+
+        public class Calculator
+        {
+            public static bool AreEqual<T>(T Value1, T Value2)
+            {
+                return Value1.Equals(Value2);
+            }
+        }
+    }
+}
+
+
