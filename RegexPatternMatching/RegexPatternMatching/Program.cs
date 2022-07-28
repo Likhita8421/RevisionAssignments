151e5b9d416059f35161d380c34f55b8314adbf1
Likhita <leenupandagre24@gmail.com>
2022-07-28 18:36:46 +0530

    Regular Expression

---
+++ RegexPatternMatching/RegexPatternMatching/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexPatternMatching
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            Console.WriteLine(patterns.validatePinCode("abc123"));
+            Console.WriteLine(patterns.validatePinCode("400123"));
+
+        }
+
+    }
+}
