bce053b15a179d92d4d15271b13f9be4cc143cf5
Likhita <leenupandagre24@gmail.com>
2022-07-29 14:54:29 +0530

    Exceptions

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class HandleException
+    {   
+       public static void Main()
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"C:\Users\hp\Documents\RevisionAssignments\ExceptionHandling\ExceptionHandling\TextFile.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine("Please Check if the file exists");
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                Console.WriteLine("Finally Block");
+
+            }
+        }
+
+    }
+}
