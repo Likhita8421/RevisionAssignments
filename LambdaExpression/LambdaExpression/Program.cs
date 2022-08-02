fd9f24a9da6996aae63207b263a41701f36a61b0
Likhita <leenupandagre24@gmail.com>
2022-08-02 18:28:52 +0530

    Lambda Expression

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Person> listPersonInCity = new List<Person>();
+            AddRecords(listPersonInCity);
+            RetrieveRecordsLessThanForty(listPersonInCity);
+        }
+
+        private static void AddRecords(List<Person> listPersonInCity)
+        {
+            listPersonInCity.Add(new Person(101, "Leenu", 25));
+            listPersonInCity.Add(new Person(101, "Amit", 30));
+            listPersonInCity.Add(new Person(101, "Anita", 45));
+        }
+
+        private static void RetrieveRecordsLessThanForty(List<Person> listPersonInCity)
+        {
+            foreach (Person person in listPersonInCity.FindAll( e => (e.Age<=40)).ToList())
+            {
+                Console.WriteLine("Name : "+ person.Name + " \t\tAge: "+ person.Age);
+            }
+        }
+
+
+    }
+
+
+}
